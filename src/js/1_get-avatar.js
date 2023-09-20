60cf7c5aaf8c51b7ccb73b066a60aad5ee1ce5ab
Mayte <mayte710@hotmail.com>
2023-09-20 13:42:58 +0100

    fix errir get-avatar

--- src/js/1_get-avatar.js
+++ src/js/1_get-avatar.js
@@ -6,28 +6,25 @@ const fileField = document.querySelector('.js__profile-upload-btn');
 const profileImage = document.querySelector('.js__profile-image');
 const profilePreview = document.querySelector('.js__profile-preview');
 
-
 /**
  * Recoge el archivo añadido al campo de tipo "file"
- * y lo carga en nuestro objeto FileReader para que 
+ * y lo carga en nuestro objeto FileReader para que
  * lo convierta a algo con lo que podamos trabajar.
  * Añade un listener al FR para que ejecute una función
  * al tener los datos listos
- * @param {evento} e 
+ * @param {evento} e
  */
-function getImage(e){
+function getImage(e) {
   const myFile = e.currentTarget.files[0];
   fr.addEventListener('load', writeImage);
   fr.readAsDataURL(myFile);
 }
-
 
 /**
  * Una vez tenemos los datos listos en el FR podemos
  * trabajar con ellos ;)
  */
 function writeImage() {
-
   /* En la propiedad `result` de nuestro FR se almacena
    * el resultado. Ese resultado de procesar el fichero que hemos cargado
    * podemos pasarlo como background a la imagen de perfil y a la vista previa
@@ -37,7 +34,6 @@ function writeImage() {
   profilePreview.style.backgroundImage = `url(${fr.result})`;
   data.photo = fr.result;
 }
-
 
 /**
  * Añadimos los listeners necesarios:
